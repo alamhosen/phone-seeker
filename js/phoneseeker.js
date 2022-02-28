93ac39d6011f385d723fd00319376b0955c409e1
Alam Hosen <alamhosen.web@gmail.com>
2022-02-28 23:54:58 +0600

    show detailes initial done

--- js/phoneseeker.js
+++ js/phoneseeker.js
@@ -19,15 +19,44 @@ const displaySearchResult = phones =>{
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="loadMealDetail(${phone.brand})" class="card h-100">
+        <div class="card h-100">
             <div class="card-body">
             <img src="${phone.image}" class="card-img-top img-fluid" alt="img">
             <h5 class="card-title">Brand: ${phone.brand}</h5>
             <h5 class="card-title">Phone: ${phone.phone_name}</h5>
-            <button type="button" class="btn btn-primary">Detailes</button>
+            <button onclick="loadPhoneDetail('${phone.slug}')" type="button" class="btn btn-primary">Detailes</button>
             </div>
         </div>
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadPhoneDetail = id =>{
+    // console.log(id)
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    console.log(url);
+    fetch(url)
+    .then (res => res.json())
+    .then (data => displayPhoneDetail(data.data))
+}
+
+// loadPhoneDetail();
+
+const displayPhoneDetail = phone =>{
+    const phoneDetails = document.getElementById('phone-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${phone.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">Phone: ${phone.name}</h5>
+    <h5 class="card-title">Release Date: ${phone.releaseDate}</h5>
+        <h4>Main Features</h4>
+        <h5 class="card-title">ChipSet: ${phone.mainFeatures.chipSet}</h5>
+        <h5 class="card-title">DisplaySize: ${phone.mainFeatures.displaySize}</h5> 
+        <h5 class="card-title">Memory: ${phone.mainFeatures.memory}</h5> 
+    </div>
+    `;
+    phoneDetails.appendChild(div);
 }
