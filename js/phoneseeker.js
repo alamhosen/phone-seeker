7a46b6d0a749a9d131b70ef6d1aaccd000d53872
Alam Hosen <alamhosen.web@gmail.com>
2022-03-01 18:57:52 +0600

    sensor updated

--- js/phoneseeker.js
+++ js/phoneseeker.js
@@ -14,8 +14,9 @@ const searchPhone = () =>{
 const displaySearchResult = phones =>{
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
+    phones.slice(0, 20);
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -51,11 +52,20 @@ const displayPhoneDetail = phone =>{
     <img src="${phone.image}" class="card-img-top" alt="...">
     <div class="card-body">
     <h5 class="card-title">Phone: ${phone.name}</h5>
-    <h5 class="card-title">Release Date: ${phone.releaseDate}</h5>
-        <h4>Main Features</h4>
+    <h5 class="card-title">Release Date: ${phone?.releaseDate}</h5>
+        <h4 class="card-title mt-3">Main Features</h4>
         <h5 class="card-title">ChipSet: ${phone.mainFeatures.chipSet}</h5>
         <h5 class="card-title">DisplaySize: ${phone.mainFeatures.displaySize}</h5> 
         <h5 class="card-title">Memory: ${phone.mainFeatures.memory}</h5> 
+        <h5 class="card-title">Storage: ${phone.storage}</h5>
+        <h4 class="card-title mt-3">Others Info</h4>
+        <h5 class="card-title">Bluetooth: ${phone.others?.Bluetooth}</h5> 
+        <h5 class="card-title">GPS: ${phone.others?.GPS}</h5> 
+        <h5 class="card-title">NFC: ${phone.others?.NFC}</h5> 
+        <h5 class="card-title">Radio: ${phone.others?.Radio}</h5> 
+        <h5 class="card-title">USB: ${phone.others?.USB}</h5> 
+        <h5 class="card-title">WLAN: ${phone.others?.WLAN}</h5>
+        <h5 class="card-title">Sensors: ${phone.mainFeatures?.sensors}</h5>
     </div>
     `;
     phoneDetails.appendChild(div);
